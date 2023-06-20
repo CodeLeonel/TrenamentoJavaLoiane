0fd3c619b3affe4acf7b5b3ac950712d4ab79344
Hugo Leonel <hugoleonel.code@gmail.com>
2023-06-20 06:11:54 -0300

    Annotations

---
+++ 65.Annotations/src/aula65/Aula65.java
@@ -0,0 +1,23 @@
+package aula65;
+
+@InformacaoAula(
+		autor = "Loiane",
+		aulaNumero = 65
+)
+public class Aula65 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 3 tipos de Anotações
+		 * 
+		 *  Informações para o compilador - Lombok ??
+		 *  
+		 *  Runtime (tempo de execução) - Hibernate
+		 *  
+		 *  Compile ou Deploy-time (tempo de compilação ou deploy - web) - Configuração Spring
+		 */
+		
+	}
+	
+}
