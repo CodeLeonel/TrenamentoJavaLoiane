4dd4ecd93535e3435032f543990478378cdf8fdd
Hugo Leonel <hugoleonel.code@gmail.com>
2023-06-20 00:06:35 -0300

    Escopo de Variaveis

---
+++ 60.EscopoDeVariaveis/src/aula60/Aula60.java
@@ -0,0 +1,20 @@
+package aula60;
+
+public class Aula60 {
+
+	public static void main(String[] args) {
+		
+		EscopoVariaveis escopo = new EscopoVariaveis();
+		
+		escopo.setValor(10);
+		
+		System.out.println(escopo.getValor());
+		
+		System.out.println(escopo.calculaValor(20));
+		
+		System.out.println(escopo.getValor());
+		
+
+	}
+
+}
