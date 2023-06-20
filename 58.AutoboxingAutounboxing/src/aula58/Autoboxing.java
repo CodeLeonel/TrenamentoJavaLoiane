54dcbae32bce13aa24e4f430ffda517776c769e2
Hugo Leonel <hugoleonel.code@gmail.com>
2023-06-19 23:15:22 -0300

    Autoboxing e Autounboxing

---
+++ 58.AutoboxingAutounboxing/src/aula58/Autoboxing.java
@@ -0,0 +1,18 @@
+package aula58;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+
+		Short var1 = 1;
+		Byte var2 = 10;
+		Integer var3 = 100;
+		Long var4 = 1000L;
+		Float var5 = 20f;
+		Double var6 = 40.0;
+		Boolean var7 = true;
+		Character a = 'a';
+		
+	}
+
+}
---
+++ 58.AutoboxingAutounboxing/src/aula58/Autounboxing.java
@@ -0,0 +1,27 @@
+package aula58;
+
+public class Autounboxing {
+
+	public static void main(String[] args) {
+		
+		Short var1 = 1;
+		Byte var2 = 10;
+		Integer var3 = 100;
+		Long var4 = 1000L;
+		Float var5 = 20f;
+		Double var6 = 40.0;
+		Boolean var7 = true;
+		Character var8 = 'a';
+		
+		short var9 = var1;
+		byte var10 = var2;
+		int var11 = var3;
+		long var12 = var4;
+		float var13 = var5;
+		double var14 = var6;
+		boolean var15 = var7;
+		char var16 = var8;
+
+	}
+
+}
